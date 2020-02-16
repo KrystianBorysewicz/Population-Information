fb9143c41b813eb73ff6061a68c23f2bc3df4b0d
Krystian Borysewicz <omnitored@gmail.com>
2020-02-16 21:12:59 +0000

    Flexible sql function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,36 @@
+import java.io.Console;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        printQueryResult("SELECT * FROM country order by Population desc limit 5");
+        printQueryResult("SELECT * FROM city limit 3");
+
+
+
+    }
+    public static void printQueryResult(String query) throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/world?serverTimezone=UTC",
                 "root","password123");
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM country order by Population desc");
+
+
+        ResultSet rs = st.executeQuery(query);
         ResultSetMetaData rsmd = rs.getMetaData();
         int columnsNumber = rsmd.getColumnCount();
         while (rs.next()) {
+            String s = "";
             for (int i = 1; i <= columnsNumber; i++) {
-                if (i > 1) System.out.print(",  ");
                 String columnValue = rs.getString(i);
-                System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
+                s += (rsmd.getColumnName(i) + ": " + columnValue + " ");
             }
-            System.out.println("");
+            System.out.println(s);
         }
         st.close();
+
+        return;
     }
 }
